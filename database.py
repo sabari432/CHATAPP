db69a9457d0bf139c86b4b4f998ec56400e25e71
sabaree <sabareeswaran.n26@gmail.com>
2026-03-04 15:56:17 +0530

    Fix Python version and SSL for MongoDB

--- database.py
+++ database.py
@@ -1,12 +1,18 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import ssl
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
+
 db = client.chatapp
 
 users_collection = db["users"]
